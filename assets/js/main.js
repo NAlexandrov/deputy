4ca5d023b00725b5a7836e4a17252bd4d26441f6
Egor Eremeev <ogogo.com@gmail.com>
2013-10-02 09:52:11 +0400

    Revert "Revert "no message""
    This reverts commit ed410668aa4d3f8eac52ef960f05947a59dbae40.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
         $(this).parent().parent().children('.more').toggle();
     });
 
-    $('#field_43096796').mask('+7 (999) 999-9999');
-    $('#field_43550358').mask('+7 (999) 999-9999');
+    if ( $('#field_43096796').length ) $('#field_43096796').mask('+7 (999) 999-9999');
+    if ( $('#field_43550358').length ) $('#field_43550358').mask('+7 (999) 999-9999');
 
     $('input[type="checkbox"]').on('click', function() {
         $(this).parent().parent().parent().find('button').toggleClass('disabled');
@@ -37,16 +37,23 @@ $(document).ready(function() {
             $(element).closest('.form-group').removeClass('has-error');
         },
         submitHandler: function(form) {
-            var params = jQuery.param({
+            /*var params = jQuery.param({
                 name: $('#field_43096795').val(),
                 email: $('#field_43096794').val(),
                 phone: $('#field_43096796').val()
-            });
+            });*/
 
             $(form).find('button').button('loading');
 
             $.post('https://podio.com/webforms/5537565/434647', $(form).serialize()).always(function() {
-                window.location.href = "card.php?"+params;
+                //window.location.href = "card.php?"+params;
+                var card = $('<form method="post" action="card.php" style="display: none">'
+                    +'<input type="hidden" name="name" value="'+$('#field_43096795').val()+'">'
+                    +'<input type="hidden" name="email" value="'+$('#field_43096794').val()+'">'
+                    +'<input type="hidden" name="phone" value="'+$('#field_43096796').val()+'">'
+                    +'</form>');
+                $('body').append(card);
+                $(card).submit();
                 $(form).find('button').button('reset');
             });
         }
@@ -75,16 +82,23 @@ $(document).ready(function() {
             $(element).closest('.form-group').removeClass('has-error');
         },
         submitHandler: function(form) {
-            var params = jQuery.param({
+            /*var params = jQuery.param({
                 name: $('#field_43550356').val(),
                 email: $('#field_43550357').val(),
                 phone: $('#field_43550358').val()
-            });
+            });*/
 
             $(form).find('button').button('loading');
 
             $.post('https://podio.com/webforms/5594624/439028', $(form).serialize()).always(function() {
-                window.location.href = "card.php?"+params;
+                //window.location.href = "card.php?"+params;
+                var card = $('<form method="post" action="card.php" style="display: none">'
+                    +'<input type="hidden" name="name" value="'+$('#field_43550356').val()+'">'
+                    +'<input type="hidden" name="email" value="'+$('#field_43550357').val()+'">'
+                    +'<input type="hidden" name="phone" value="'+$('#field_43550358').val()+'">'
+                    +'</form>');
+                $('body').append(card);
+                $(card).submit();
                 $(form).find('button').button('reset');
             });
         }
